bd80c4e74530735034ca29f819126978ee0738d5
dikhayeva00 <dikhayeva00@gmail.com>
2021-05-22 16:53:07 +0900

    Apply design to dashboard component

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,8 +1,72 @@
 import React from 'react';
+import { Box, Text, Center, Wrap, Flex, Circle } from '@chakra-ui/react';
+import { AddIcon } from '@chakra-ui/icons';
 
 const Dashboard = ({ data }) => {
-  console.log(data);
-  return <h1> dashboard </h1>;
+  const dummy = [
+    { id: '123' },
+    { id: '345' },
+    { id: '567' },
+    { id: '789' },
+    { id: '987' },
+  ];
+  const wrapItems = () =>
+    dummy.map(item => (
+      <Flex
+        as="button"
+        direction="column"
+        align="center"
+        _hover={{ bg: 'gray.100', borderRadius: 20 }}
+        _active={{
+          bg: '#dddfe2',
+          transform: 'scale(0.98)',
+          borderColor: '#bec3c9',
+        }}
+        key={item.id}
+        p={2}
+      >
+        <Box
+          w="240px"
+          h="160px"
+          border="1px"
+          borderColor="gray.200"
+          borderRadius={20}
+          boxShadow="inner"
+          mb="3"
+        />
+        <Text fontSize={16}>Hello</Text>
+      </Flex>
+    ));
+  return (
+    <Center>
+      <Box w={1000} mt={50}>
+        <Text fontSize={36} fontWeight="bold" mb={50} pl={10}>
+          Dashboard
+        </Text>
+        <Wrap spacing="50px" shouldWrapChildren={true} justify="center">
+          {wrapItems()}
+        </Wrap>
+        <Flex justify="flex-end">
+          <Circle
+            as="button"
+            bgGradient="linear(to-r, #6C7789, #4A5568)"
+            w="80px"
+            h="80px"
+            m={30}
+            boxShadow="lg"
+            _hover={{ bg: 'gray.600', borderRadius: 40 }}
+            _active={{
+              bg: '#dddfe2',
+              transform: 'scale(0.98)',
+              borderColor: '#bec3c9',
+            }}
+          >
+            <AddIcon color="white" fontSize={20} />
+          </Circle>
+        </Flex>
+      </Box>
+    </Center>
+  );
 };
 
 export default Dashboard;
