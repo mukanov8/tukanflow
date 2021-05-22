c1226cb70ed06b68e37f83335bd3af83ab0046b5
BaurTokenov <baur.tokenov@kaist.ac.kr>
2021-05-22 11:15:36 +0600

    feat: CKeditor priehal

--- src/pages/editor/Editor.js
+++ src/pages/editor/Editor.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import styled from 'styled-components';
+import CKEditor from './CKEditorWrapper';
+
+const MainContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  padding-top: 90px;
+`;
 
 const Editor = ({ data }) => {
-  console.log(data);
-  return <h1> Editor </h1>;
+  console.log('arrow function uspokoisya');
+
+  return (
+    <MainContainer>
+      <CKEditor {...{ data }} />;
+    </MainContainer>
+  );
 };
 
 export default Editor;
